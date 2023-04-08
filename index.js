860ea0ae808031da084c4dfa813076729dfc272a
Ryan <salvat36@gmail.com>
2023-04-08 05:40:23 -0500

    passing lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+let hq = 42;
+function distanceFromHqInBlocks(pickUp) {
+return Math.abs(pickUp - hq);
+}
+
+function distanceFromHqInFeet(pickUp) {
+return distanceFromHqInBlocks(pickUp) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+let totalFeet = Math.abs(start - destination)
+return totalFeet * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    if (distanceInFeet < 400) {
+    return 0;
+    } else if (distanceInFeet >= 400 && distanceInFeet < 2000) {
+    return (distanceInFeet - 400) * .02;
+    } else if (distanceInFeet <= 2500) {
+    return 25;
+    } else {
+    return 'cannot travel that far';
+    }
+}
